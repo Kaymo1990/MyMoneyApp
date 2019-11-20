c6845083ce74650a8aaa20031061e7892f9fb3ea
Kay <khurrammohammed1990@gmail.com>
2019-11-20 16:21:13 +0000

    added migration for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence: true,
+        uniqueness: { case_sensitive: false }
+    validates :password_digest, presence: true
+    validates :salary, presence: true
+    validates :start, presence: true
+    validates :end, presence: true
 end
--- db/migrate/20191120115805_create_users.rb
+++ db/migrate/20191120115805_create_users.rb
@@ -3,9 +3,9 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.int :salary
-      t.int :start
-      t.int :end
+      t.integer :salary
+      t.integer :start
+      t.integer :end
 
       t.timestamps
     end
