90259f2aff8d1df8c89805741258f17e17f6bf84
Kay <khurrammohammed1990@gmail.com>
2019-11-20 22:55:38 +0000

    login working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,50 @@
 class UsersController < ApplicationController
+
+  protect_from_forgery
+
+  def show
+    if logged_in?
+      @user = User.find(params[:id])
+      @posts = Post.all
+    else
+      redirect_to sessions_new_url
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+ 
   def new
+      @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      log_in(@user)
+      # session[:current_user_id] = @user.id
+      # redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  @user = User.find(params[:id])
+  if @user.update_attributes(user_params)
+    redirect_to user_path(@user.id)
+    # Handle a successful update.
+  else
+    render 'edit'
+  end
+end
+
+  def user_params
+    params.require(:user).permit(:username, :salary, :start, :end, :password)
   end
 end
