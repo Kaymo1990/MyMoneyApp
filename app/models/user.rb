47484ea553bb1cbf86129a746be083b74977d6a2
Kay <khurrammohammed1990@gmail.com>
2019-11-20 18:18:31 +0000

    user authentication working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+    before_save { self.username = username.downcase! }
     validates :username, presence: true,
         uniqueness: { case_sensitive: false }
-    validates :password_digest, presence: true
-    validates :salary, presence: true
-    validates :start, presence: true
-    validates :end, presence: true
+        validates :salary, presence: true
+        validates :start, presence: true
+        validates :end, presence: true
+        validates :password_digest, presence: true, length: { minimum: 6 }
+    has_secure_password
 end
