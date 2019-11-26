e4fa94e3c34f9f90526c89a459c7d652f6d91f08
Kay <khurrammohammed1990@gmail.com>
2019-11-26 01:30:56 +0000

    login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
   def create
     if
       user = User.find_by(username: params[:session][:username].downcase)
-    if user && user.authenticate(params[:session][:password])
-      #login
-    else
-      render 'new'
+      if user && user.authenticate(params[:session][:password])
+        log_in user
+        redirect_to user
+      else
+        render 'new'
+      end
     end
   end
 
